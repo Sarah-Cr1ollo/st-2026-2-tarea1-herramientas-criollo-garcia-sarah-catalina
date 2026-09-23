4b4315ce3911ca8cd8bdfdc58b7f55565d178c24
Sarah Catalina Criollo <scriollog@unal.edu.co>
2026-09-22 22:33:30 -0500

    Implementa ajustar_media() con calculo recursivo

--- R/02-metodos.R
+++ R/02-metodos.R
@@ -1,11 +1,13 @@
-# Media simple (recursiva) 
+#  buscamos la media simple (recursiva) 
 ajustar_media <- function(y) {
+  #usamos is.numeric(y) para convierte a y en un vector
+  # necesitamos que !any(is.na(y) para que no aparezca ningun NA
   stopifnot(is.numeric(y), !any(is.na(y)), length(y) >= 2)
-  
+  # serie con al menos 2 obser.
   n <- length(y)
   yhat <- rep(NA_real_, n)
   yhat[2] <- y[1]
-  
+  # esta funcion la exigue el enuenciado 
   ybarra <- y[1]
   if (n >= 3) {
     for (t in 2:(n - 1)) {
@@ -15,7 +17,7 @@ ajustar_media <- function(y) {
   }
   # ybarra final = media de toda la muestra de estimacion
   ybarra_final <- mean(y)
-  
+  # con este bloque vamos a calcular la media  de toda la serie
   pronosticar <- function(h) {
     stopifnot(h >= 1)
     rep(ybarra_final, h)
